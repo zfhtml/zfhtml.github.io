08c7dc956c148b5cb910c4aafeec253b2f3d7c62
zfhtml <785715785@qq.com>
2026-02-06 22:10:14 +0800

    Add files via upload

---
+++ 01play/m3u8Links.js
@@ -0,0 +1,7 @@
+
+
+var m3u8Links = [
+    "https://long.longyuandingyi.com/videos1/4edd0382aa28a9a297dc66ae21d0edd4/4edd0382aa28a9a297dc66ae21d0edd4.m3u8?auth_key=1696350438-0-0-ff6170369a5d5d95ebd166c92d28c3ae&v=2",
+
+"https://long.longyuandingyi.com/watch8/6f3adbf0968d60b77c7b6b9e96d7c8ef/6f3adbf0968d60b77c7b6b9e96d7c8ef.m3u8?auth_key=1696350445-0-0-98ad0957f25821e1df18498a5118f6fd&v=2"
+];
